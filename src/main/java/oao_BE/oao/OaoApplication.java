b66538d6b7338241ba83ad0bc7138ca4d7663a51
shinminkyoung1 <dottiee0702@gmail.com>
2025-08-08 16:52:08 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/oao_BE/oao/OaoApplication.java
@@ -0,0 +1,13 @@
+package oao_BE.oao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oao_BE/oao/OaoApplicationTests.java
@@ -0,0 +1,13 @@
+package oao_BE.oao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
